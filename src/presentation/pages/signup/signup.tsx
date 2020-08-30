c11cc83c00578a3d2cb425e2a3baa42873c46c7a
Victor Poletti <victorpoletti2009@hotmail.com>
2020-08-30 11:25:30 -0300

    feat: ensure Login link goes to the right place

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -124,7 +124,12 @@ const SignUp: React.FC<Props> = ({
             Entrar
           </button>
 
-          <Link to="/login" className={Styles.link}>
+          <Link
+            data-testid="login-link"
+            replace
+            to="/login"
+            className={Styles.link}
+          >
             Voltar para login
           </Link>
           <FormStatus />
