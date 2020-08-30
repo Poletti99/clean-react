4661200378c58c341c6c693257f06aa7d0cc6d7d
Victor Poletti <victorpoletti2009@hotmail.com>
2020-08-30 09:17:10 -0300

    test: ensure SignUp change passwordConfirmation status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -43,7 +43,7 @@ describe('SignUp Page', () => {
     Helper.testStatusForField(sut, 'name', validationError);
     Helper.testStatusForField(sut, 'email', validationError);
     Helper.testStatusForField(sut, 'password', validationError);
-    Helper.testStatusForField(sut, 'passwordConfirmation', 'Campo obrigatório');
+    Helper.testStatusForField(sut, 'passwordConfirmation', validationError);
   });
 
   test('Should show name error if Validation fails', () => {
@@ -68,5 +68,13 @@ describe('SignUp Page', () => {
 
     Helper.populateField(sut, 'password');
     Helper.testStatusForField(sut, 'password', validationError);
+  });
+
+  test('Should show passwordConfirmation error if Validation fails', () => {
+    const validationError = faker.random.words();
+    const { sut } = makeSut({ validationError });
+
+    Helper.populateField(sut, 'passwordConfirmation');
+    Helper.testStatusForField(sut, 'passwordConfirmation', validationError);
   });
 });
