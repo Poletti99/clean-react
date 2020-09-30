4256adb1935e740878f4cd175d35c88994343099
Victor Poletti <victorpoletti2009@hotmail.com>
2020-09-30 07:35:38 -0300

    test: ensure SignUp prevent submit if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -109,4 +109,13 @@ describe('SignUp', () => {
     cy.getByTestId('submit').dblclick();
     FormHelper.testHttpCallsCount(1);
   });
+
+  it('should not call submit if form is invalid', () => {
+    Http.mockOk();
+    cy.getByTestId('email')
+      .focus()
+      .type(faker.internet.email())
+      .type('{enter}');
+    FormHelper.testHttpCallsCount(0);
+  });
 });
